001d3885d135fd2ead64b8245b3730f82f819c7d
jackintoshh <jjwright10@hotmail.com>
2018-02-14 16:33:23 +0000

    Tune class private fields public accessors

---
+++ ie/dit/Tune.java
@@ -0,0 +1,20 @@
+package ie.dit;
+import java.io.IOException;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+
+public class Tune{
+	private int x;
+	private String title, altTitle, notation;
+
+	public Tune(int x, String title, String altTitle, String notation)
+	{
+		this.x = x;
+		this.title = title;
+		this.altTitle = altTitle;
+		this.notation = notation
+	}
+
+	
+}
